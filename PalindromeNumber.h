51d2695571b040b5e58a64ef30fc1d8ef4c42765
Kristen wang <wqlgyw@163.com>
2017-02-21 10:55:21 +0800

    Update and rename PalindromeNumber to PalindromeNumber.h

--- PalindromeNumber.h
+++ PalindromeNumber.h
@@ -1,13 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x < 0){
+/*negative integer is not palindrome*/
+        if(x < 0){                  
             return false;
         }
+/*find the highest position,which is the number of bits*/
         int rec = 1;
         while(x / rec >= 10){
            rec = rec * 10; 
         }
+/*define two variable,one represents the beginning,the other is the end*/
         while(x != 0){
             int i = x / rec;
             int j = x % 10;
