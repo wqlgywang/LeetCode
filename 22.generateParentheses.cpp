7662e5192fb1980cd685f0646c7f731d55d0bbf4
Kristen wang <wqlgyw@163.com>
2017-02-27 14:11:50 +0800

    Update 22.generateParentheses.cpp
    Medium

--- 22.generateParentheses.cpp
+++ 22.generateParentheses.cpp
@@ -1,3 +1,13 @@
+/*Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses. 
+For example, given n = 3, a solution set is: 
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]*/
+/*wrong answer*/
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
@@ -5,7 +15,7 @@ public:
         generate(n,n," ",res);
         return res;
     }
-    vector<string> generate(int left,int right, string out,vector<string> &res){
+    void generate(int left,int right, string out,vector<string> &res){
         if(left ==0 && right == 0) res.push_back(out);
         else if(left > 0 && left <= right){
             generate(left-1,right,out+"(",res);
@@ -13,6 +23,23 @@ public:
         else if(right > 0 && left < right){
             generate(left,right-1,out+")",res);
         }
+    }
+};
+/*a little change makes the right answer,pay attention to the difference between if...if and if..else if */
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        generate(n,n,"",res);
         return res;
+    }
+    /*left and right are the remaining number of "(" and ")"*/
+    void generate(int left,int right, string out,vector<string> &res){
+        if (left > right) return;
+        if (left == 0 && right == 0) res.push_back(out);
+        else {
+            if (left > 0) generate(left - 1, right, out + '(', res);
+            if (right > 0) generate(left, right - 1, out + ')', res);
+        }
     }
 };
