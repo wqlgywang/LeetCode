b50bb794268277eb6db1196bc1eb7f0454935f41
Kristen wang <wqlgyw@163.com>
2017-03-01 14:11:33 +0800

    Update and rename 1.TwoSum.h to 1.TwoSum.cpp
    Easy

--- 1.TwoSum.cpp
+++ 1.TwoSum.cpp
@@ -1,3 +1,10 @@
+/*Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+*/
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -10,6 +17,7 @@ public:
         sort(numscopy.begin(), numscopy.end());
         vector<int> copyres = compare(numscopy, target);
         
+        /*pay attention to the two for loops' order,which is fist and which is second*/
         for(int i = 0;i < length;i++){
             for(int j = 0;j < copyres.size();j++){
                 if(copyres[j] == nums[i]){
@@ -18,12 +26,13 @@ public:
                 }
             }
         }
-        if (result[0]>result[1]){
-            //reverse(result.begin(),result.end());
-            result[0] = result[0]^result[1];
-            result[1] = result[0]^result[1];
-            result[0] = result[0]^result[1];
-        }
+        //if (result[0]>result[1]){
+        //    result[0] = result[0]^result[1];
+        //    result[1] = result[0]^result[1];
+        //    result[0] = result[0]^result[1];
+        //}
+        /*the last 4 lines is equal to the sort()function*/
+        sort(result.begin(),result.end());
         return result;
     }
      vector<int> compare(vector<int> &numscp, int target){
