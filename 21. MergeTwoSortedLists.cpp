65fe800ce6a4da220170096af9a6766f10b103ea
Kristen wang <wqlgyw@163.com>
2017-03-09 10:52:53 +0800

    Update 21. MergeTwoSortedLists.cpp
    Easy

--- 21. MergeTwoSortedLists.cpp
+++ 21. MergeTwoSortedLists.cpp
@@ -42,6 +42,7 @@ public:
                 p = l1;
                 l1 = l1->next;
                 p = p->next;
+
             }
         }
         while(l1!=NULL){
@@ -116,3 +117,54 @@ public:
         return head;
     }
 };
+
+
+/** 
+ * Definition for singly-linked list. 
+ * struct ListNode { 
+ *     int val; 
+ *     ListNode *next; 
+ *     ListNode(int x) : val(x), next(NULL) {} 
+ * }; 
+ */  
+class Solution {  
+public:  
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {  
+        ListNode * head = new ListNode(0);  
+        ListNode * ptr = head;  
+          
+        while(l1 != NULL || l2 != NULL){  
+            ListNode * cur = NULL;  
+            if(l1 == NULL){  
+                cur = l2;  
+                l2 = l2->next;  
+            }  
+            else if(l2 == NULL){  
+                cur = l1;  
+                l1 = l1->next;  
+            }  
+            else{  
+                if(l1->val < l2->val){  
+                    cur = l1;  
+                    l1 = l1->next;  
+                }  
+                else{  
+                    cur = l2;  
+                    l2 = l2->next;  
+                }  
+            }  
+            ptr->next = cur;  
+            ptr = ptr->next;  
+        }  
+          
+        return head->next;  
+    }  
+};  
+
+
+
+
+
+
+
+
