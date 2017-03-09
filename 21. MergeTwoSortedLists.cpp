143b70e1297f918acf819942105654feddd08cc9
Kristen wang <wqlgyw@163.com>
2017-03-09 10:23:07 +0800

    Update 21. MergeTwoSortedLists.cpp

--- 21. MergeTwoSortedLists.cpp
+++ 21. MergeTwoSortedLists.cpp
@@ -3,8 +3,7 @@ The new list should be made by splicing together the nodes of the first two list
 
 /*by myself,but has fault:
 Input:
-[2]
-[1]
+[2][1]
 Output:
 [1]
 Expected:
@@ -14,13 +13,11 @@ Expected:
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
+ *     ListNode(int x) : val(x), next(NULL) {} * };
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode *head,*p;
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {        ListNode *head,*p;
         if(l1==NULL && l2==NULL) return NULL;
         if(l1 == NULL) return l2;
         if(l2 == NULL) return l1;
@@ -55,6 +52,64 @@ public:
             p = l2;
             l2 = l2->next;
             p = p ->next;
+        }
+        return head;
+    }
+};
+/*a l*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode *head=NULL,*p=NULL;
+        if(l1==NULL && l2==NULL) return NULL;
+        if(l1 == NULL) return l2;
+        if(l2 == NULL) return l1;
+        
+        if(l2->val <= l1->val){
+            head=l2;
+            l2 = l2->next;
+        }
+        else{
+            head = l1;
+            l1 = l1->next;
+        }
+        p = head;
+        while(l1!=NULL && l2!=NULL){
+            
+            if(l2->val<=l1->val){
+                p->next = l2;
+                l2 = l2->next;
+               
+            }
+            else{
+                p->next = l1;
+                l1 = l1->next;
+            }
+            p = p->next;
+        }
+        if(l2==NULL){
+            while(l1!=NULL){
+                
+                p->next = l1;
+                l1 = l1->next;
+                p = p->next;
+            }
+        }
+        if(l1==NULL){
+            while(l2!=NULL){
+                
+                p->next = l2;
+                l2 = l2->next;
+                p = p ->next;
+            }
         }
         return head;
     }
